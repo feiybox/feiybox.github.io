8224756981e82cd58abf30879c1027e99ed37f98
feiybox <feiybox@foxmail.com>
2019-03-16 17:44:49 +0800

    Site updated: 2019-03-16 17:44:46

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 16 2019 16:59:21 GMT+0800 (China Standard Time)
+// build time:Sat Mar 16 2019 17:37:53 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
