2403d5bb9e4281ca10c6bdfa422cd1fd37ffc0f3
feiybox <feiybox@foxmail.com>
2019-04-07 21:53:03 +0800

    Site updated: 2019-04-07 21:53:01

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Apr 05 2019 23:47:59 GMT+0800 (China Standard Time)
+// build time:Sun Apr 07 2019 21:51:42 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
