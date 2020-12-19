b0ca32cfdd5dddc15a584d44310ca4bea00819ca
feiybox <feiybox@foxmail.com>
2020-12-19 15:51:43 +0800

    Site updated: 2020-12-19 15:51:38

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 08 2020 23:25:24 GMT+0800 (China Standard Time)
+// build time:Sat Dec 19 2020 15:51:16 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
