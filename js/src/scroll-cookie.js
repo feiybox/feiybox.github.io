8bd14afa979039b628495dcec1669b2a1a8303e1
feiybox <feiybox@foxmail.com>
2021-06-29 23:56:34 +0800

    Site updated: 2021-06-29 23:56:31

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat May 22 2021 22:58:08 GMT+0800 (China Standard Time)
+// build time:Tue Jun 29 2021 23:55:11 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
